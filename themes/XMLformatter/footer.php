086da31487975188f75f0582c313f2031ef49856
Alex Marica <alex.marica@russmedia.com>
2017-04-11 15:36:59 +0300

    asd

--- themes/XMLformatter/footer.php
+++ themes/XMLformatter/footer.php
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
+
+ 
+ <footer>
+ 	
+=======
+>>>>>>> 088bb53fe9e62de2e340827daa6e31db7cb9e5f7
 
 <footer>
 </footer>
--- themes/XMLformatter/header.php
+++ themes/XMLformatter/header.php
@@ -4,6 +4,21 @@
 <meta charset="utf-8">	
 	<title> XMLFomatter</title>
 <nav>
+<<<<<<< HEAD
+	<div class="menu-centered spacem " data-sticky-container>
+	  <ul class="menu align-center sticky " data-sticky data-margin-top="0">
+	<li class="sticky" data-sticky data-anchor="foo"><h2><?php wp_nav_menu ($args); ?></h2></li>
+	</ul>
+	<!-- <div class="menu-centered spacem ">
+	  <ul class="menu align-center ">
+	    <li><h2><a href="#">Home</a></h2></li>
+	    <li><h2><a href="#">About app</a></h2></li>
+	    <li><h2><a href="#">Package</a></h2></li>
+	  
+	  </ul> -->
+	</div>
+</nav>
+=======
 	<div class="menu-centered spacem ">
 	  <ul class="menu align-center ">
 	<li><h2><?php wp_nav_menu ($args); ?></h2></li>
@@ -12,5 +27,6 @@
 </nav>
 
 
+>>>>>>> 088bb53fe9e62de2e340827daa6e31db7cb9e5f7
 </head>
 	
