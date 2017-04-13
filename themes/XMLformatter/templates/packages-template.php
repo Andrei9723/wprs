81249f1e618467baec1b53bd9773e11ea0c96c8b
Alex Marica <alex.marica@russmedia.com>
2017-04-13 11:53:22 +0300

    asd

--- themes/XMLformatter/templates/packages-template.php
+++ themes/XMLformatter/templates/packages-template.php
@@ -4,15 +4,7 @@ Template Name: Packages
 */
 ?>
 <?php  get_header();?> 
-
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		<?php the_content(__('(more...)')); ?>
-
-	<hr><?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php endif; ?>
+<div class="packageimage">
+<?php the_post_thumbnail( 'full' ); ?>
 </div>
-	<?php get_sidebar(); ?>
-</div>
-	<div id="delimiter">
-	</div>
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
