f862331d808d181058825a03a276186117035243
Alex Marica <alex.marica@russmedia.com>
2017-04-20 09:22:38 +0300

    modal

--- themes/XMLformatter/templates/packages-template.php
+++ themes/XMLformatter/templates/packages-template.php
@@ -40,11 +40,50 @@ Template Name: Packages
 	  <li><a class="button" href="#">Buy Now</a></li>
 	</ul>
 </div>
+<h2>Modal Example</h2>
+
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
 </body>
-		<footer class="footer-basic-centered bottomfoot">
 
-			<p class="footer-company-motto">XMLformatter , it's as simple as that!</p>
-
-			<p class="footer-company-name">RussMediaTech &copy; 2017</p>
-
-		</footer>
+<?php get_footer(); ?>
